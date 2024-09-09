6e25f14909e4277beee323cd0c93f8b068e7c28f
michel <michel@re-invention.nl>
2024-09-09 08:32:37 +0200

    chore: initial commit
    semi working version

---
+++ src/message/handlers/mod.rs
@@ -0,0 +1,4 @@
+pub mod excluded_search_phrases;
+pub mod login;
+pub mod privileged_users;
+pub mod room_list;
---
+++ src/message/mod.rs
@@ -0,0 +1,7 @@
+pub mod factory;
+pub mod handlers;
+pub mod message;
+pub mod reader;
+
+pub use message::*;
+pub use reader::*;
