66593388a1e5dc79849b1c15efea4e3c614b829b
michel <michel@re-invention.nl>
2025-01-05 20:05:47 +0100

    chore: can processs default peer , message: 9

--- src/message/server/connect_to_peer.rs
+++ src/message/server/connect_to_peer.rs
@@ -1,4 +1,5 @@
 use crate::message::{Message, MessageHandler};
+use crate::peer::peer::Peer;
 use crate::server::ServerOperation;
 use std::sync::mpsc::Sender;
 pub struct ConnectToPeerHandler;
@@ -6,35 +7,8 @@ impl MessageHandler for ConnectToPeerHandler {
     fn get_code(&self) -> u8 {
         18
     }
-    fn handle(&self, _message: &mut Message, _sender: Sender<ServerOperation>) {
-        println!("Handling ConnectToPeer message");
-        // let username = message.read_string();
-        // let connection_type = message.read_string();
-        // let mut ip: Vec<i8> = vec![];
-        // for _ in 0..4 {
-        //     ip.push(message.read_int8());
-        // }
-        // let host: String = format!("{}.{}.{}.{}", ip[3], ip[2], ip[1], ip[0]);
-        //
-        // let (port, token, privileged, unknown, obfuscated_port) = (
-        //     message.read_int32(),
-        //     message.read_int32(),
-        //     message.read_int8(),
-        //     message.read_int8(),
-        //     message.read_int8(),
-        // );
-        //
-        // let peer = Peer::new(
-        //     username,
-        //     connection_type,
-        //     host,
-        //     port,
-        //     token,
-        //     privileged,
-        //     unknown,
-        //     obfuscated_port,
-        // );
-        //
-        // sender.send(ServerOperation::ConnectToPeer(peer)).unwrap();
+    fn handle(&self, message: &mut Message, sender: Sender<ServerOperation>) {
+        let peer = Peer::new_from_message(message);
+        sender.send(ServerOperation::ConnectToPeer(peer)).unwrap();
     }
 }
